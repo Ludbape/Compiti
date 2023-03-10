bfc2ca216ba3eb013922a185f04d149a487acea6
Abdullah Ijaz <abdullahijaz2912@gmail.com>
2023-03-10 16:42:24 +0100

    Esercizio con le stringhe

---
+++ Stringhe/impiccato.py
@@ -0,0 +1,19 @@
+def impiccato(stringa: str) -> str:
+    stringa = stringa.lower()
+    stringa_risultante = ''
+    vocali = 'aeiou'
+    for lettera in stringa:
+        if lettera in vocali:
+            stringa_risultante += '+'
+        else:
+            stringa_risultante += '-'
+            
+    return stringa_risultante
+            
+            
+def main():
+    s = input('Inserisci la parola da indovinare: ')
+    print(impiccato(s))
+    
+if __name__ == '__main__':
+    main()
