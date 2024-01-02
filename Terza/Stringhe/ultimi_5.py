975e04a80a7b41c5addbb672f1390b1edb008521
Abdullah Ijaz <abdullahijaz2912@gmail.com>
2024-01-02 12:24:21 +0100

    Ultimi esercizi sulle stringhe

--- Terza/Stringhe/ultimi_5.py
+++ Terza/Stringhe/ultimi_5.py
@@ -1,12 +1,5 @@
-
 if __name__ == '__main__':
     stringa = "Ciao mondo"
     stringa = stringa.replace("o", "k")
     for i in stringa[-5:]:
-        print(i)
-
-
-
-
-
-
+        print(i.upper())
